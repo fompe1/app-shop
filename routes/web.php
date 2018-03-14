9cdc47361ccc3b673224d9893687acc7beb45be2
unknown <fompe123@gmail.com>
2018-03-14 05:40:49 -0600

    9.Sistema de autenticacion

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::get('/', 'TestController@welcome');
 Route::get('/prueba',function (){
     return 'Hola soy la ruta prueba';
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
